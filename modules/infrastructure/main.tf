41b241f71c6d42082d58440601ff819ef497b589
cappetta <thomas.cappetta@gmail.com>
2018-09-09 14:58:42 -0400

    removing db from the provisioning

--- modules/infrastructure/main.tf
+++ modules/infrastructure/main.tf
@@ -3,17 +3,17 @@ module "network" {
 
   environment = "${var.environment}"
 }
-
-module "databases" {
-  source = "modules/databases"
-
-  environment = "${var.environment}"
-
-  vpc-id              = "${module.network.vpc-id}"
-  vpc-cidr-block      = "${module.network.vpc-cidr-block}"
-  private-a-subnet-id = "${module.network.private-a-subnet-id}"
-  private-b-subnet-id = "${module.network.private-b-subnet-id}"
-}
+//
+//module "databases" {
+//  source = "modules/databases"
+//
+//  environment = "${var.environment}"
+//
+//  vpc-id              = "${module.network.vpc-id}"
+//  vpc-cidr-block      = "${module.network.vpc-cidr-block}"
+//  private-a-subnet-id = "${module.network.private-a-subnet-id}"
+//  private-b-subnet-id = "${module.network.private-b-subnet-id}"
+//}
 
 module "instances" {
   source = "modules/instances"
