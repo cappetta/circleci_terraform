9f3ba6e42be86a95e8e2c780a2160035521df161
cappetta <thomas.cappetta@gmail.com>
2018-09-10 01:15:12 -0400

    updating docker-webgoat

--- modules/infrastructure/modules/instances/docker.tf
+++ modules/infrastructure/modules/instances/docker.tf
@@ -42,7 +42,7 @@ resource "null_resource" "docker" {
     inline = [
       "sudo yum -y install docker",
       "sudo service docker start",
-      "sudo curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose",
+      "sudo curl -L 'https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)' -o /usr/local/bin/docker-compose",
       "curl https://raw.githubusercontent.com/WebGoat/WebGoat/develop/docker-compose.yml | sdocker-compose -f - up"
     ]
   }
