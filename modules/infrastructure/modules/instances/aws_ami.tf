603db08f0c8bb26f19a16a90cccc920c5b5b1563
cappetta <thomas.cappetta@gmail.com>
2018-09-09 23:49:09 -0400

    adding new ami's for centos, kali, amazon linux 2

--- modules/infrastructure/modules/instances/aws_ami.tf
+++ modules/infrastructure/modules/instances/aws_ami.tf
@@ -18,8 +18,8 @@ data "aws_ami" "kali" {
   most_recent = true
 
   filter {
-    name   = "name"
-    values = ["*kali-*"]
+    name   = "description"
+    values = ["Kali Linux*"]
   }
 
   filter {
@@ -31,4 +31,38 @@ data "aws_ami" "kali" {
 }
 
 
+data "aws_ami" "amazon-linux-2" {
+  most_recent = true
 
+
+  filter {
+    name   = "owner-alias"
+    values = ["amazon"]
+  }
+
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm*"]
+  }
+}
+
+data "aws_ami" "centos" {
+  owners      = ["679593333241"]
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["CentOS Linux 7 x86_64 HVM EBS *"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+}
