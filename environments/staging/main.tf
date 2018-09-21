5c822c1178e2d6f4c62f52ecf08e01f83ef5abe4
cappetta <thomas.cappetta@gmail.com>
2018-09-20 22:56:55 -0400

    fixing ci error w/ terraform output

--- environments/staging/main.tf
+++ environments/staging/main.tf
@@ -20,12 +20,12 @@ module "staging-infrastructure" {
   source      = "../../modules/infrastructure"
   environment = "${var.environment}"
 }
-
-output "web-alb-dns-name" {
-  value = "${module.staging-infrastructure.web-alb-dns-name}"
-}
-
-output "web-instance-ips" {
-  value = "${module.staging-infrastructure.web-instance-ips}"
-}
+//
+//output "web-alb-dns-name" {
+//  value = "${module.staging-infrastructure.web-alb-dns-name}"
+//}
+//
+//output "web-instance-ips" {
+//  value = "${module.staging-infrastructure.web-instance-ips}"
+//}
 
